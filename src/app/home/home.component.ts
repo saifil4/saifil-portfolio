7236ad2f28ba2c5b01028b94a6f036554e2876d1
LAPTOP-HJ9F78UR\saifi <saifilmomin4@gmail.com>
2020-10-05 20:33:40 -0400

    changes in home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {ProjectListComponent} from '../project-list/project-list.component';
+import { ProjectListComponent } from '../project-list/project-list.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
-
+  title = 'Home';
   constructor() { }
 
   ngOnInit(): void {
