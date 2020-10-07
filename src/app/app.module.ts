f726e40cc982076cddc1de8ef56f85038aabd9e8
LAPTOP-HJ9F78UR\saifi <saifilmomin4@gmail.com>
2020-10-07 02:02:21 -0400

    changes in WFC page, font styling, favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { WheelForChangeComponent } from './wheel-for-change/wheel-for-change.component';
 import { FooterComponent } from './footer/footer.component';
 
-
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about-me', component: AboutMeComponent },
